02d206c7719b41fa5c579648ffd42634c4f4141f
mpokiabel <mpokiabel@gmail.com>
2023-05-12 16:38:10 +0200

    testing

--- scripts/extract.c
+++ scripts/extract.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/stat.h>
-#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
@@ -17,44 +16,47 @@ int main(int argc, char const *argv[])
     printf("length is %ld\n", fdS_stat.st_size);
 
     int pipefd[2];
+    pid_t pid = getpid();
 
     if (pipe(pipefd) == -1)
     {
         perror("pipe");
         return -1;
     }
+    printf("Process ID %d, pipe read end %d, pipe write end %d, destination FD %d\n", pid, pipefd[0], pipefd[1], fdD);
+    long off_in = 52, off_out = 0;
+    size_t num_blocks = 500;
+    int sp, x = 0;
 
-    long off_in = 0;
-    long off_out = 0;
-    int sp, x;
+    // while (x < num_blocks)
+    // {
+    //     sp = splice(fdS, &off_in, pipefd[1], NULL, 202, SPLICE_F_MORE);
+    //     if (sp == 0)
+    //     {
+    //         printf("End of input\n");
+    //         break;
+    //     }
 
-    while (1)
+    //     off_in = off_in + 22 + 52;
+
+    //     x++;
+    //     printf("IN WHILE %d\n", sp);
+    // }
+    while (x<2)
     {
-        off_in = off_in + 52;
-        sp = splice(fdS, &off_in, pipefd[1], NULL, 202, SPLICE_F_MOVE);
-
-        if (sp == 0)
-            break;
-
-        off_out = off_out + sp;
-        off_in = off_in + 22 + 200;
+        sp = splice(fdS, &off_in, pipefd[1], NULL, fdS_stat.st_size, SPLICE_F_MORE);
+        printf("spliced data size %d\n",sp);
 
         x++;
     }
+    
 
     sp = splice(pipefd[0], NULL, fdD, &off_out, sp, SPLICE_F_MOVE);
-
-    // while (x < 5)
-    // {
-    //     sp = splice(fdS, NULL, pipefd[1], NULL, 204+50+20+2, SPLICE_F_MOVE);
-    //     x++;
-    // }
-    // sp = sp * (x+1);
-    // sp = splice(pipefd[0], NULL, fdD, NULL, sp, SPLICE_F_MOVE);
 
     close(pipefd[0]);
     close(pipefd[1]);
     close(fdS);
     close(fdD);
+
     return 0;
 }
